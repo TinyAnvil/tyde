98b6b1ef2d36ad50a4f873e378531d9c544fa691
Tyler van der Hoeven <hi@tyvdh.com>
2015-12-11 18:03:06 -0500

    updated meteor, made some improvements

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,20 +1,24 @@
-var shrink = function() {
+function shrink() {
   $('.message-list .message-item').each(function() {
     var $self = $(this);
-    var width = $(this).find('.date').width() + 15;
+    var width = $self.find('.date').width() + 15;
 
     if ($self.hasClass('left')) {
-      $(this).find('.message').attr('style', 'transform: translate('+ -width +'px,0); -webkit-transform: translate('+ -width +'px,0); -moz-transform: translate('+ width +'px,0);');
+      $self.find('.message').attr('style', 
+        'transform: translate('+ -width +'px,0);'+
+        '-webkit-transform: translate('+ -width +'px,0);'+
+        '-moz-transform: translate('+ width +'px,0);');
     } else if ($self.hasClass('right')) {
-      $(this).find('.message').attr('style', 'transform: translate('+ width +'px,0); -webkit-transform: translate('+ width +'px,0); -moz-transform: translate('+ width +'px,0);');
+      $self.find('.message').attr('style', 
+        'transform: translate('+ width +'px,0);'+
+        '-webkit-transform: translate('+ width +'px,0);'+
+        '-moz-transform: translate('+ width +'px,0);');
     }
   });
 }
 
 Template.home.rendered = function() {
   $('.message-form textarea').autosize();
-
-  shrink();
 
   window.addEventListener('native.keyboardshow', keyboardShowHandler);
   window.addEventListener('native.keyboardhide', keyboardHideHandler);
@@ -25,6 +29,11 @@ Template.home.rendered = function() {
   function keyboardHideHandler(e) {
     $('body').css('height', '100%');
   }
+
+  Tracker.autorun(function() {
+    Messages.find().fetch();
+    shrink();
+  });
 }
 
 Template.home.events({
@@ -38,11 +47,7 @@ Template.home.events({
       sentBy: Meteor.userId(),
       sentAt: new Date
     }, function(error, id) {
-      if (error) return false;
-
       $('.text', event.target).val('');
-      shrink();
-      return id;
     });
 
     event.preventDefault();
@@ -51,7 +56,7 @@ Template.home.events({
 
 Template.home.helpers({
   messages: function() {
-    return Messages.find({},{sort:{sentAt: 1}});
+    return Messages.find({}, {sort: {sentAt: 1}});
   },
   position: function(sentBy) {
     if (Meteor.userId() == sentBy) {
